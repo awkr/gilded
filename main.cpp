ebd72473c2a845f1580ee7f27ea4221af7043e47
Hongjian Zhu <zhu.life@gmail.com>
2025-11-02 21:18:47 +0800

    add keyboard & mouse event handler

--- main.cpp
+++ main.cpp
@@ -27,11 +27,38 @@ void destroy_window(GLFWwindow *window) {
     glfwTerminate();
 }
 
+void handle_keyboard_input(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void handle_mouse_position(GLFWwindow *window, double x, double y) {
+}
+
+void handle_mouse_button(GLFWwindow *window, int button, int action, int mods) {
+}
+
+void handle_mouse_scroll(GLFWwindow *window, double xoffset, double yoffset) {
+}
+
+void init_input(GLFWwindow *window) {
+    glfwSetKeyCallback(window, handle_keyboard_input);
+    glfwSetCursorPosCallback(window, handle_mouse_position);
+    glfwSetMouseButtonCallback(window, handle_mouse_button);
+    glfwSetScrollCallback(window, handle_mouse_scroll);
+}
+
+void poll_events(GLFWwindow *window) {
+}
+
 void run(const Config &config) {
     GLFWwindow *window;
     create_window(config, &window);
+    init_input(window);
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        poll_events(window);
     }
     destroy_window(window);
 }
